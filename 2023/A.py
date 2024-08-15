e36d14a076417e2c4c102cf542755a883cfa630b
Alikinhas <lucasgalvao134@gmail.com>
2024-08-15 11:01:08 -0300

    QA - 2023

--- 2023/A.py
+++ 2023/A.py
@@ -1,1 +1,10 @@
-print ("Questão A")
+n, h = input().split()
+n = int(n)
+h = int(h)
+a = [int (i) for i in input().split()]
+n = 0
+for i in a:
+    if i <= h:
+        n = n + 1
+
+print (n)        
